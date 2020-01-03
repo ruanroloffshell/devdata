d3aec04b5982ad692f2f8f3c7618804c33df118e
ruanroloff <ruanroloff@gmail.com>
2020-01-03 00:48:42 -0300

    alterado identi

--- Code/word.py
+++ Code/word.py
@@ -7,20 +7,20 @@ from pyspark.sql import SparkSession
 if __name__ == "__main__":
 
     spark = SparkSession\
-        .builder\
-        .appName("PythonWordCount")\
-        .getOrCreate()
+		.builder\
+		.appName("PythonWordCount")\
+		.getOrCreate()
 
 	sc = spark.sparkContext
 
 	words = "he quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog"
-    lines = sc.parallelize(words)
-    counts = lines.flatMap(lambda x: x.split(' ')) \
-                  .map(lambda x: (x, 1)) \
-                  .reduceByKey(add)
+	lines = sc.parallelize(words)
+	counts = lines.flatMap(lambda x: x.split(' ')) \
+		.map(lambda x: (x, 1)) \
+		.reduceByKey(add)
 				  
-    output = counts.collect()
-    for (word, count) in output:
+	output = counts.collect()
+	for (word, count) in output:
         print("%s: %i" % (word, count))
 
-    spark.stop()
+	spark.stop()
